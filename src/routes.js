3761ae8768bf017b181cadfccb32d5cd108175cd
CIT\fernandosimoes <fernandosimoes@ciandt.com>
2019-04-12 15:44:22 -0300

    atualização do repositorio

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const multer = require('multer');
+const multerConfig  = require('./config/multer');
 
 const router = express.Router();
 
 const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
+
+router.get('/', (req, res)=>{
+    res.send('servidor de pé');
+})
 
 router.post('/boxes', BoxController.store);
+router.post('/files', multer(multerConfig).single('file') , FileController.store);
 
 
 module.exports = router;
